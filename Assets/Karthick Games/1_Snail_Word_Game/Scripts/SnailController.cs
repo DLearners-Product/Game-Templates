ef26dffb4a6161e53b04123a4902c12b4e27174b
karthickdl <karthick@dlearners.in>
2023-03-29 19:25:30 +0530

    Daily commit 29/03/2023

--- Assets/Karthick Games/1_Snail_Word_Game/Scripts/SnailController.cs
+++ Assets/Karthick Games/1_Snail_Word_Game/Scripts/SnailController.cs
@@ -7,29 +7,21 @@ public class SnailController : MonoBehaviour
 {
     public Vector3 direction;
     public float snailSpeed;
-    public float rayDepth = 5f;
-
+    public float rayDistance;
     public RectTransform characterRectTransform;
     public Transform fallDetection;
 
-    public LayerMask borderLayer;
-
-
-
-
-    public float moveSpeed = 5f;
-    public float blockDetectDistance = 0.5f;
-
-    private Rigidbody2D rb;
-    private bool movingRight = true;
-    private Transform leftBlock;
-    private Transform rightBlock;
+    private LayerMask borderLayer;
+    private Vector3 localScale;
 
     // Start is called before the first frame update
     void Start()
     {
+        localScale = characterRectTransform.localScale;
+
         direction = Vector3.right;
         snailSpeed = 0.01f;
+        rayDistance = 0.5f;
     }
 
 
@@ -60,97 +52,36 @@ public class SnailController : MonoBehaviour
 
     public void CheckAndMove()
     {
+        RaycastHit2D hit = Physics2D.Raycast(characterRectTransform.position, direction, rayDistance, borderLayer);
 
-
-        transform.Translate(direction * snailSpeed);
-
-
-
-
-
-
-
-        /*        RaycastHit2D frontHit = Physics2D.Raycast(characterRectTransform.position, direction, rayDepth, borderLayer);
-                RaycastHit2D backHit = Physics2D.Raycast(characterRectTransform.position, -direction, rayDepth, borderLayer);
-
-                Debug.Log("front " + frontHit);
-                Debug.Log("front collider" + frontHit.collider);
-                Debug.Log(frontHit.collider == null);
-
-                Debug.Log("back " + backHit);
-                Debug.Log("back collider " + backHit.collider);
-
-                if (frontHit != true && frontHit.collider.gameObject.CompareTag("TileBody"))
+        /*
+                if (hit.collider.CompareTag("TileBody"))
                 {
-                    Debug.Log("front");
+                    //hits a collider and that collider is TileBody
+                    transform.Translate(direction * snailSpeed);
+                    Debug.DrawRay(characterRectTransform.position, direction * rayDistance, Color.green);
 
-                    Debug.DrawRay(characterRectTransform.position, direction, Color.green);
-                    ChangeDirection();
-                    return;
                 }
-                else if (backHit != true && backHit.collider.gameObject.CompareTag("TileBody"))
+                else if (!hit.collider.CompareTag("TileBody"))
                 {
-                    Debug.Log("back");
-
-                    Debug.DrawRay(characterRectTransform.position, -direction, Color.red);
-                    ChangeDirection();
-                    return;
-                }
-
-                transform.Translate(direction * snailSpeed);*/
+                    //collider detected | dont move
+                    //transform.Translate(direction * snailSpeed);
+                    Debug.DrawRay(characterRectTransform.position, direction * rayDistance, Color.red);
 
 
-        /*if (frontHit.collider != null)
-        {
-            if (frontHit.collider.CompareTag("TileBody"))
-            {
-                if (direction == Vector3.left)
-                {
-                    direction = Vector3.right;
-                    characterRectTransform.localScale = new Vector3(1, 1, 1);
                 }
                 else
                 {
-                    direction = Vector3.left;
-                    characterRectTransform.localScale = new Vector3(-1, 1, 1);
-                }
-            }
+                    //no collider detected | move
+                    transform.Translate(direction * snailSpeed);
+                    Debug.DrawRay(characterRectTransform.position, direction * rayDistance, Color.green);
+                }*/
 
 
-            Debug.DrawRay(characterRectTransform.position, direction, Color.green);
-            transform.Translate(direction * snailSpeed);
-        }
-        else
-        {
-
-        }*/
-
-
-        /* Ray downray = new Ray(fallDetection.position, Vector3.down);
-         RaycastHit downHitInfo;
-         if (Physics.Raycast(downray, out downHitInfo, rayDepth))
-         {
-             //if ray hits Wall, don't move the player
-             if (!downHitInfo.collider.CompareTag("TileBody"))
-             {
-                 Debug.Log("bottom ray hitting");
-
-                 return;
-             }
-         }*/
-
-
-        transform.Translate(direction * snailSpeed);
-    }
-
-    void LateUpdate()
-    {
-        CheckWhereToFace();
-    }
-
-    void CheckWhereToFace()
-    {
+       
 
     }
+
+
 }
 
