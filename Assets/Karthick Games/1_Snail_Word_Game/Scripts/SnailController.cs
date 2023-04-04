41698a11805976dfb21ea664e6e60fdc08b8371d
Jaikishore <jaikishore@dlearners.in>
2023-04-04 19:58:21 +0530

    snail movement fix implemented

--- Assets/Karthick Games/1_Snail_Word_Game/Scripts/SnailController.cs
+++ Assets/Karthick Games/1_Snail_Word_Game/Scripts/SnailController.cs
@@ -8,20 +8,23 @@ public class SnailController : MonoBehaviour
     public Vector3 direction;
     public float snailSpeed;
     public float rayDistance;
-    public RectTransform characterRectTransform;
-    public Transform fallDetection;
+    public RectTransform frontRay;
+    public RectTransform backRay;
+    public bool renderRight, renderLeft;
 
-    private LayerMask borderLayer;
+    [SerializeField] private LayerMask borderLayer;
     private Vector3 localScale;
+    float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        localScale = characterRectTransform.localScale;
+        localScale = frontRay.localScale;
 
         direction = Vector3.right;
         snailSpeed = 0.01f;
         rayDistance = 0.5f;
+        speed = snailSpeed;
     }
 
 
@@ -33,53 +36,55 @@ public class SnailController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("TileBody"))
+        // if (collision.gameObject.CompareTag("TileBody"))
+        // {
+        //     ChangeDirection();
+        // }
+    }
+
+    private void ChangeDirection()
+    {
+        if (direction == Vector3.left)
         {
-            if (direction == Vector3.left)
-            {
-                direction = Vector3.right;
-                characterRectTransform.localScale = new Vector3(1, 1, 1);
+            Debug.Log($"Directed to right");
+            direction = Vector3.right;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            Debug.Log($"Directed to left");
+            direction = Vector3.left;
+            transform.localScale = new Vector3(-1, 1, 1);
 
-            }
-            else
-            {
-                direction = Vector3.left;
-                characterRectTransform.localScale = new Vector3(-1, 1, 1);
-
-            }
+            // characterRectTransform.localScale = transform.q(new Vector3(-1, 1, 1));
         }
     }
 
     public void CheckAndMove()
     {
-        RaycastHit2D hit = Physics2D.Raycast(characterRectTransform.position, direction, rayDistance, borderLayer);
+        transform.Translate(direction * speed);
 
-        /*
-                if (hit.collider.CompareTag("TileBody"))
-                {
-                    //hits a collider and that collider is TileBody
-                    transform.Translate(direction * snailSpeed);
-                    Debug.DrawRay(characterRectTransform.position, direction * rayDistance, Color.green);
+        RaycastHit2D hitLeft = Physics2D.Raycast(frontRay.position, direction, rayDistance, borderLayer);
 
-                }
-                else if (!hit.collider.CompareTag("TileBody"))
-                {
-                    //collider detected | dont move
-                    //transform.Translate(direction * snailSpeed);
-                    Debug.DrawRay(characterRectTransform.position, direction * rayDistance, Color.red);
+        if(renderRight)
+            Debug.DrawRay(frontRay.position, direction * rayDistance, Color.green);
 
+        Vector2 opposite = new Vector2(direction.x * -1, direction.y);
 
-                }
-                else
-                {
-                    //no collider detected | move
-                    transform.Translate(direction * snailSpeed);
-                    Debug.DrawRay(characterRectTransform.position, direction * rayDistance, Color.green);
-                }*/
+        Debug.Log($"{opposite}");
 
+        RaycastHit2D hitRight = Physics2D.Raycast(backRay.position, opposite, rayDistance, borderLayer);
 
-       
+        if(renderLeft)
+            Debug.DrawRay(backRay.position, opposite * rayDistance, Color.green);
 
+        if(hitLeft.collider != null && hitRight.collider != null){
+            speed = 0f;
+            Debug.Log($"Don't move");
+        }else if(hitLeft.collider != null){
+            speed = snailSpeed;
+            ChangeDirection();
+        }
     }
 
 
